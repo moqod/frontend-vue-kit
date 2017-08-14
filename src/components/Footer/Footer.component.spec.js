9947bf298ca24db41012f3d0655ff4f001d316a3
Юрий Дунаев <ukolsd@gmail.com>
2017-08-14 11:20:49 +0400

    In work

--- src/components/Footer/Footer.component.spec.js
+++ src/components/Footer/Footer.component.spec.js
@@ -7,23 +7,23 @@ describe('FooterComponent', () => {
   // Inspect the raw component options
   it('has a created hook', () => {
     // expect(typeof FooterComponent.created).toBe('function');
-  })
+  });
   // Evaluate the results of functions in
   // the raw component options
   it('sets the correct default data', () => {
     // expect(typeof FooterComponent.data).toBe('function')
     // const defaultData = FooterComponent.data();
     // expect(defaultData.message).toBe('hello!');
-  })
+  });
   // Inspect the component instance on mount
   it('correctly sets the message when created', () => {
     // const vm = new Vue(FooterComponent).$mount();
     // expect(vm.message).toBe('bye!');
-  })
+  });
   // Mount an instance and inspect the render output
   it('renders the correct message', () => {
     // const Ctor = Vue.extend(FooterComponent);
     // const vm = new Ctor().$mount();
     // expect(vm.$el.textContent).toBe('bye!');
-  })
-})
+  });
+});
